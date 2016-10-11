89bd657fc845577eba1a8c6b6982d830ebaf9ecd
drdr xp <drdr.xp@gmail.com>
2016-10-11 19:33:56 +0800

    build online web doc with gitbook

--- build.sh
+++ build.sh
@@ -1,12 +1,60 @@
 #!/bin/sh
 
-for i in $(cd src; find . -name "*.md"); do
+# requires:
+#
+#   nodejs
+#       $ brew install nodejs
+#
+#   gitbook
+#       $ npm install gitbook
+#
+#   ebook-convert
+#       install calibre.app
+#       $ sudo ln -s /Applications/calibre.app/Contents/MacOS/ebook-convert /usr/bin
+#
+#   doctoc # to generate table-of-content
+#       $ npm install doctoc
+
+die()
+{
+    echo "$@" >&2
+    exit 1
+}
+
+rm -rf building
+mkdir building || die mkdir building
+
+# adding toc changes src files
+cp -R src/* building/ || die copy to building/
+
+# add table-of-content to every *.md
+for i in $(cd building; find . -name "*.md" | grep -v "SUMMARY\|README"); do
+
+    doctoc "building/$i" || die adding TOC to "$i"
+
+done
+
+# build online web doc
+while read pth; do
+
+    gitbook build -f web building/$pth dist/$pth || die build with gitbook "$pth"
+
+done <<-END
+imgx
+s2
+END
+
+
+exit $?
+
+
+for i in $(cd building; find . -name "*.md"); do
 
     pandoc --standalone         \
-        -H src/_inc/css.html    \
+        -H building/_inc/css.html    \
         -f markdown -t html     \
         --toc --toc-depth=5     \
-        src/$i                  \
+        building/$i                  \
         --output dist/${i%.*}.html
 
 done
